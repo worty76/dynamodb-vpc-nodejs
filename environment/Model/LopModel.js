0d95743afcc24ef8630f685d155678351cffa818
Phatdat01 <phatdatlht2001@gmail.com>
2022-05-05 10:55:08 +0700

    Lop-PhanCong-SinhVienModel

--- environment/Model/LopModel.js
+++ environment/Model/LopModel.js
@@ -7,132 +7,173 @@ const AWS = require('aws-sdk');
 
 // Configure the region
 AWS.config.update({
-  region: 'us-east-1',
+    region: 'us-east-1',
 });
 
 const sqs = new AWS.SQS({ apiVersion: '2012-11-05' });
 var docClient = new AWS.DynamoDB.DocumentClient();
 async function Get() {
-  const params = {
-    TableName: 'Lops',
-  };
-  try {
-    let Records = await docClient.scan(params).promise();
-    return Records;
-  } catch (error) {
-    console.log(error);
-  }
+    const params = {
+        TableName: 'Lops',
+    };
+    try {
+        let Records = await docClient.scan(params).promise();
+        return Records;
+    } catch (error) {
+        console.log(error);
+    }
 }
 async function Create(Data) {
-  let LopData = {
-    MaLop: Data.MaLop,
-    TenLop: Data.TenLop,
-    MaKhoa: Data.MaKhoa,
-  };
-  let sqsLopData = {
-    MessageAttributes: {
-      MaLop: {
-        DataType: 'String',
-        StringValue: LopData.MaLop,
-      },
-      TenLop: {
-        DataType: 'String',
-        StringValue: LopData.TenLop,
-      },
-      MaKhoa: {
-        DataType: 'String',
-        StringValue: LopData.MaKhoa,
-      },
-    },
-    MessageBody: JSON.stringify(LopData),
-    QueueUrl: 'https://sqs.us-east-1.amazonaws.com/' + accountId + '/InsertLop',
-  };
-  let sendSqsMessage = sqs.sendMessage(sqsLopData).promise();
-  sendSqsMessage
-    .then((data) => {
-      console.log(`OrdersSvc | SUCCESS: ${data.MessageId}`);
-      return;
-    })
-    .catch((err) => {
-      console.log(`OrdersSvc | ERROR: ${err}`);
+    let LopData = {
+        MaLop: Data.MaLop,
+        TenLop: Data.TenLop,
+        MaKhoa: Data.MaKhoa,
+    };
+    let sqsLopData = {
+        MessageAttributes: {
+            MaLop: {
+                DataType: 'String',
+                StringValue: LopData.MaLop,
+            },
+            TenLop: {
+                DataType: 'String',
+                StringValue: LopData.TenLop,
+            },
+            MaKhoa: {
+                DataType: 'String',
+                StringValue: LopData.MaKhoa,
+            },
+        },
+        MessageBody: JSON.stringify(LopData),
+        QueueUrl: 'https://sqs.us-east-1.amazonaws.com/' + accountId + '/InsertLop',
+    };
+
+    var params = {
+        TableName: 'Lops',
+        Item: LopData,
+    };
+
+    docClient.put(params, function(err, data) {
+        if (err) console.log(err);
+        else console.log('Insert successfully: ' + data);
     });
+
+    let sendSqsMessage = sqs.sendMessage(sqsLopData).promise();
+    sendSqsMessage
+        .then((data) => {
+            console.log(`OrdersSvc | SUCCESS: ${data.MessageId}`);
+            return;
+        })
+        .catch((err) => {
+            console.log(`OrdersSvc | ERROR: ${err}`);
+        });
 }
 async function GetById(ID) {
-  var params = {
-    TableName: 'Lops',
-    FilterExpression: '#name = :value',
-    ExpressionAttributeValues: { ':value': ID },
-    ExpressionAttributeNames: { '#name': 'MaLop' },
-  };
-  try {
-    const data = await docClient.scan(params).promise();
-    return data;
-  } catch (error) {
-    console.log(error);
-  }
+    var params = {
+        TableName: 'Lops',
+        FilterExpression: '#name = :value',
+        ExpressionAttributeValues: { ':value': ID },
+        ExpressionAttributeNames: { '#name': 'MaLop' },
+    };
+    try {
+        const data = await docClient.scan(params).promise();
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
 }
 async function Update(ID, TenLop, MaKhoa) {
-  let LopData = {
-    MaLop: ID,
-    TenLop: TenLop,
-    MaKhoa: MaKhoa,
-  };
-  let sqsLopData = {
-    MessageAttributes: {
-      MaLop: {
-        DataType: 'String',
-        StringValue: LopData.MaLop,
-      },
-      TenLop: {
-        DataType: 'String',
-        StringValue: LopData.TenLop,
-      },
-      MaKhoa: {
-        DataType: 'String',
-        StringValue: LopData.MaKhoa,
-      },
-    },
-    MessageBody: JSON.stringify(LopData),
-    QueueUrl: 'https://sqs.us-east-1.amazonaws.com/' + accountId + '/UpdateLop',
-  };
-  let sendSqsMessage = sqs.sendMessage(sqsLopData).promise();
-  sendSqsMessage
-    .then((data) => {
-      console.log(`OrdersSvc | SUCCESS: ${data.MessageId}`);
-      return;
-    })
-    .catch((err) => {
-      console.log(`OrdersSvc | ERROR: ${err}`);
+    let LopData = {
+        MaLop: ID,
+        TenLop: TenLop,
+        MaKhoa: MaKhoa,
+    };
+    let sqsLopData = {
+        MessageAttributes: {
+            MaLop: {
+                DataType: 'String',
+                StringValue: LopData.MaLop,
+            },
+            TenLop: {
+                DataType: 'String',
+                StringValue: LopData.TenLop,
+            },
+            MaKhoa: {
+                DataType: 'String',
+                StringValue: LopData.MaKhoa,
+            },
+        },
+        MessageBody: JSON.stringify(LopData),
+        QueueUrl: 'https://sqs.us-east-1.amazonaws.com/' + accountId + '/UpdateLop',
+    };
+
+    var params = {
+        TableName: 'Lops',
+        Key: {
+            MaLop: ID,
+        },
+        UpdateExpression: 'set MaLop = :u, TenLop=:r,MaKhoa=:q',
+        ExpressionAttributeValues: {
+            ':u': Data.MaLop,
+            ':r': Data.TenLop,
+            ':q': Data.MaKhoa,
+        },
+    };
+
+    docClient.update(params, function(err, data) {
+        if (err) console.log(err);
+        else console.log('Update MaLop, TenLop,MaKhoa: ' + data);
     });
+
+    let sendSqsMessage = sqs.sendMessage(sqsLopData).promise();
+    sendSqsMessage
+        .then((data) => {
+            console.log(`OrdersSvc | SUCCESS: ${data.MessageId}`);
+            return;
+        })
+        .catch((err) => {
+            console.log(`OrdersSvc | ERROR: ${err}`);
+        });
 }
 async function Delete(ID) {
-  let LopData = {
-    MaLop: ID,
-  };
-  let sqsLopData = {
-    MessageAttributes: {
-      MaLop: {
-        DataType: 'String',
-        StringValue: LopData.MaLop,
-      },
-    },
-    MessageBody: JSON.stringify(LopData),
-    QueueUrl: 'https://sqs.us-east-1.amazonaws.com/' + accountId + '/DeleteLop',
-  };
-  let sendSqsMessage = sqs.sendMessage(sqsLopData).promise();
-  sendSqsMessage
-    .then((data) => {
-      console.log(`OrdersSvc | SUCCESS: ${data.MessageId}`);
-      return;
-    })
-    .catch((err) => {
-      console.log(`OrdersSvc | ERROR: ${err}`);
+    let LopData = {
+        MaLop: ID,
+    };
+    let sqsLopData = {
+        MessageAttributes: {
+            MaLop: {
+                DataType: 'String',
+                StringValue: LopData.MaLop,
+            },
+        },
+        MessageBody: JSON.stringify(LopData),
+        QueueUrl: 'https://sqs.us-east-1.amazonaws.com/' + accountId + '/DeleteLop',
+    };
+
+    var params = {
+        TableName: 'Lops',
+        Key: LopData,
+    };
+
+    docClient.delete(params, function(err, data) {
+        if (err) console.log(err);
+        else console.log('Delete successfully: ' + data);
     });
+
+    let sendSqsMessage = sqs.sendMessage(sqsLopData).promise();
+    sendSqsMessage
+        .then((data) => {
+            console.log(`OrdersSvc | SUCCESS: ${data.MessageId}`);
+            return;
+        })
+        .catch((err) => {
+            console.log(`OrdersSvc | ERROR: ${err}`);
+        });
 }
 module.exports = {
-  Get: Get,
-  Create: Create,
-  GetById: GetById,
-  Update: Update,
-  Delete: Delete,
+    Get: Get,
+    Create: Create,
+    GetById: GetById,
+    Update: Update,
+    Delete: Delete,
 };
--- environment/Model/SinhVienModel.js
+++ environment/Model/SinhVienModel.js
@@ -7,180 +7,229 @@ const AWS = require('aws-sdk');
 
 // Configure the region
 AWS.config.update({
-  region: 'us-east-1',
+    region: 'us-east-1',
 });
 
 //
 const sqs = new AWS.SQS({ apiVersion: '2012-11-05' });
 var docClient = new AWS.DynamoDB.DocumentClient();
 async function Get() {
-  const params = {
-    TableName: 'SinhViens',
-  };
-  try {
-    let Records = await docClient.scan(params).promise();
-    return Records;
-  } catch (error) {
-    console.log(error);
-  }
+    const params = {
+        TableName: 'SinhViens',
+    };
+    try {
+        let Records = await docClient.scan(params).promise();
+        return Records;
+    } catch (error) {
+        console.log(error);
+    }
 }
 async function Create(Data) {
-  let SVData = {
-    MaSV: Data.MaSV,
-    TenSV: Data.TenSV,
-    NgaySinh: Data.NgaySinh,
-    GioiTinh: Data.GioiTinh,
-    QueQuan: Data.QueQuan,
-    MaLop: Data.MaLop,
-  };
-  let sqsLopData = {
-    MessageAttributes: {
-      MaSV: {
-        DataType: 'String',
-        StringValue: SVData.MaSV,
-      },
-      TenSV: {
-        DataType: 'String',
-        StringValue: SVData.TenSV,
-      },
-      NgaySinh: {
-        DataType: 'String',
-        StringValue: SVData.NgaySinh,
-      },
-      GioiTinh: {
-        DataType: 'String',
-        StringValue: SVData.GioiTinh,
-      },
-      QueQuan: {
-        DataType: 'String',
-        StringValue: SVData.QueQuan,
-      },
-      MaLop: {
-        DataType: 'String',
-        StringValue: SVData.MaLop,
-      },
-    },
-    MessageBody: JSON.stringify(SVData),
-    QueueUrl: 'https://sqs.us-east-1.amazonaws.com/' + accountId + '/InsertSV',
-  };
-  let sendSqsMessage = sqs.sendMessage(sqsLopData).promise();
-  sendSqsMessage
-    .then((data) => {
-      console.log(`OrdersSvc | SUCCESS: ${data.MessageId}`);
-      return;
-    })
-    .catch((err) => {
-      console.log(`OrdersSvc | ERROR: ${err}`);
+    let SVData = {
+        MaSV: Data.MaSV,
+        TenSV: Data.TenSV,
+        NgaySinh: Data.NgaySinh,
+        GioiTinh: Data.GioiTinh,
+        QueQuan: Data.QueQuan,
+        MaLop: Data.MaLop,
+    };
+    let sqsLopData = {
+        MessageAttributes: {
+            MaSV: {
+                DataType: 'String',
+                StringValue: SVData.MaSV,
+            },
+            TenSV: {
+                DataType: 'String',
+                StringValue: SVData.TenSV,
+            },
+            NgaySinh: {
+                DataType: 'String',
+                StringValue: SVData.NgaySinh,
+            },
+            GioiTinh: {
+                DataType: 'String',
+                StringValue: SVData.GioiTinh,
+            },
+            QueQuan: {
+                DataType: 'String',
+                StringValue: SVData.QueQuan,
+            },
+            MaLop: {
+                DataType: 'String',
+                StringValue: SVData.MaLop,
+            },
+        },
+        MessageBody: JSON.stringify(SVData),
+        QueueUrl: 'https://sqs.us-east-1.amazonaws.com/' + accountId + '/InsertSV',
+    };
+
+    var params = {
+        TableName: 'SinhVien',
+        Item: SVData,
+    };
+
+    docClient.put(params, function(err, data) {
+        if (err) console.log(err);
+        else console.log('Insert successfully: ' + data);
     });
+
+    let sendSqsMessage = sqs.sendMessage(sqsLopData).promise();
+    sendSqsMessage
+        .then((data) => {
+            console.log(`OrdersSvc | SUCCESS: ${data.MessageId}`);
+            return;
+        })
+        .catch((err) => {
+            console.log(`OrdersSvc | ERROR: ${err}`);
+        });
 }
 async function GetById(ID) {
-  var params = {
-    TableName: 'SinhViens',
-    FilterExpression: '#name = :value',
-    ExpressionAttributeValues: { ':value': ID },
-    ExpressionAttributeNames: { '#name': 'MaSV' },
-  };
-  try {
-    const data = await docClient.scan(params).promise();
-    return data;
-  } catch (error) {
-    console.log(error);
-  }
+    var params = {
+        TableName: 'SinhViens',
+        FilterExpression: '#name = :value',
+        ExpressionAttributeValues: { ':value': ID },
+        ExpressionAttributeNames: { '#name': 'MaSV' },
+    };
+    try {
+        const data = await docClient.scan(params).promise();
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 async function GetById1(ID) {
-  var params = {
-    TableName: 'SinhViens',
-    FilterExpression: '#name = :value',
-    ExpressionAttributeValues: { ':value': ID },
-    ExpressionAttributeNames: { '#name': 'MaLop' },
-  };
-  try {
-    const data = await docClient.scan(params).promise();
-    return data;
-  } catch (error) {
-    console.log(error);
-  }
+    var params = {
+        TableName: 'SinhViens',
+        FilterExpression: '#name = :value',
+        ExpressionAttributeValues: { ':value': ID },
+        ExpressionAttributeNames: { '#name': 'MaLop' },
+    };
+    try {
+        const data = await docClient.scan(params).promise();
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 async function Update(ID, Data) {
-  let SVData = {
-    MaSV: ID,
-    TenSV: Data.TenSV,
-    NgaySinh: Data.NgaySinh,
-    GioiTinh: Data.GioiTinh,
-    QueQuan: Data.QueQuan,
-    MaLop: Data.MaLop,
-  };
-  let sqsLopData = {
-    MessageAttributes: {
-      MaSV: {
-        DataType: 'String',
-        StringValue: SVData.MaSV,
-      },
-      TenSV: {
-        DataType: 'String',
-        StringValue: SVData.TenSV,
-      },
-      NgaySinh: {
-        DataType: 'String',
-        StringValue: SVData.NgaySinh,
-      },
-      GioiTinh: {
-        DataType: 'String',
-        StringValue: SVData.GioiTinh,
-      },
-      QueQuan: {
-        DataType: 'String',
-        StringValue: SVData.QueQuan,
-      },
-      MaLop: {
-        DataType: 'String',
-        StringValue: SVData.MaLop,
-      },
-    },
-    MessageBody: JSON.stringify(SVData),
-    QueueUrl: 'https://sqs.us-east-1.amazonaws.com/' + accountId + '/UpdateSV',
-  };
-  let sendSqsMessage = sqs.sendMessage(sqsLopData).promise();
-  sendSqsMessage
-    .then((data) => {
-      console.log(`OrdersSvc | SUCCESS: ${data.MessageId}`);
-      return;
-    })
-    .catch((err) => {
-      console.log(`OrdersSvc | ERROR: ${err}`);
+    let SVData = {
+        MaSV: ID,
+        TenSV: Data.TenSV,
+        NgaySinh: Data.NgaySinh,
+        GioiTinh: Data.GioiTinh,
+        QueQuan: Data.QueQuan,
+        MaLop: Data.MaLop,
+    };
+    let sqsLopData = {
+        MessageAttributes: {
+            MaSV: {
+                DataType: 'String',
+                StringValue: SVData.MaSV,
+            },
+            TenSV: {
+                DataType: 'String',
+                StringValue: SVData.TenSV,
+            },
+            NgaySinh: {
+                DataType: 'String',
+                StringValue: SVData.NgaySinh,
+            },
+            GioiTinh: {
+                DataType: 'String',
+                StringValue: SVData.GioiTinh,
+            },
+            QueQuan: {
+                DataType: 'String',
+                StringValue: SVData.QueQuan,
+            },
+            MaLop: {
+                DataType: 'String',
+                StringValue: SVData.MaLop,
+            },
+        },
+        MessageBody: JSON.stringify(SVData),
+        QueueUrl: 'https://sqs.us-east-1.amazonaws.com/' + accountId + '/UpdateSV',
+    };
+
+    var params = {
+        TableName: 'SinhVien',
+        Key: {
+            MaSV: ID,
+        },
+        UpdateExpression: 'set MaSV=:msv' +
+            'TenSV = :tsv,' +
+            'NgaySinh= :ns,' +
+            'GioiTinh=:gt,' +
+            'QueQuan=:qq,' +
+            'MaLop= :ml,',
+        ExpressionAttributeValues: {
+            ':msv': Data.MaSV,
+            ':tsv': Data.TenSV,
+            ':ns': Data.Role.NgaySinh,
+            ':gt': Data.GioiTinh,
+            ':qq': Data.QueQuan,
+            ':ml': Data.MaLop,
+        },
+    };
+
+    docClient.update(params, function(err, data) {
+        if (err) console.log(err);
+        else console.log('Update TenSV, NgaySinh,GioiTinh,QueQuan,MaLop: ' + data);
     });
+
+    let sendSqsMessage = sqs.sendMessage(sqsLopData).promise();
+    sendSqsMessage
+        .then((data) => {
+            console.log(`OrdersSvc | SUCCESS: ${data.MessageId}`);
+            return;
+        })
+        .catch((err) => {
+            console.log(`OrdersSvc | ERROR: ${err}`);
+        });
 }
 async function Delete(ID) {
-  let LopData = {
-    MaSV: ID,
-  };
-  let sqsLopData = {
-    MessageAttributes: {
-      MaSV: {
-        DataType: 'String',
-        StringValue: LopData.MaSV,
-      },
-    },
-    MessageBody: JSON.stringify(LopData),
-    QueueUrl: 'https://sqs.us-east-1.amazonaws.com/' + accountId + '/DeleteSV',
-  };
-  let sendSqsMessage = sqs.sendMessage(sqsLopData).promise();
-  sendSqsMessage
-    .then((data) => {
-      console.log(`OrdersSvc | SUCCESS: ${data.MessageId}`);
-      return;
-    })
-    .catch((err) => {
-      console.log(`OrdersSvc | ERROR: ${err}`);
+    let LopData = {
+        MaSV: ID,
+    };
+    let sqsLopData = {
+        MessageAttributes: {
+            MaSV: {
+                DataType: 'String',
+                StringValue: LopData.MaSV,
+            },
+        },
+        MessageBody: JSON.stringify(LopData),
+        QueueUrl: 'https://sqs.us-east-1.amazonaws.com/' + accountId + '/DeleteSV',
+    };
+
+    var params = {
+        TableName: 'SinhVien',
+        Key: LopData,
+    };
+
+    docClient.delete(params, function(err, data) {
+        if (err) console.log(err);
+        else console.log('Delete successfully: ' + data);
     });
+
+    let sendSqsMessage = sqs.sendMessage(sqsLopData).promise();
+    sendSqsMessage
+        .then((data) => {
+            console.log(`OrdersSvc | SUCCESS: ${data.MessageId}`);
+            return;
+        })
+        .catch((err) => {
+            console.log(`OrdersSvc | ERROR: ${err}`);
+        });
 }
 module.exports = {
-  Get: Get,
-  Create: Create,
-  GetById: GetById,
-  Update: Update,
-  Delete: Delete,
-  GetById1: GetById1,
+    Get: Get,
+    Create: Create,
+    GetById: GetById,
+    Update: Update,
+    Delete: Delete,
+    GetById1: GetById1,
 };
